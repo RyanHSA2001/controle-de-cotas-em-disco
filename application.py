634e353d6f90a9f96f43f45fe4db30908c742057
Ryan Henrique <74609999+RyanHSA2001@users.noreply.github.com>
2022-10-21 02:59:40 +0000

    Função impressão quase concluída, relatório já saí formatado conforme solicitado no escopo do projeto

--- application.py
+++ application.py
@@ -3,12 +3,13 @@ Site do exercício - https://wiki.python.org.br/ListaDeExerciciosProjetos
 """
 
 import pandas as pd
+from tabulate import tabulate
 
 
 def leitura_do_arquivo():
 
   df = pd.read_csv('usuarios.txt', sep='  ', header=None, engine='python')
-  # df = tabulate(df, showindex=True, headers=df.columns)
+  
 
   return df
 
@@ -25,16 +26,33 @@ def percentual_de_uso(espacoUtilizado, espacoTotalOcupado):
   result  =  (espacoUtilizado / espacoTotalOcupado) * 100
   result = float("{:.2f}".format(result))
   return result
-  
+
+def prefixar_espaços_nas_colunas(df: pd.DataFrame, n):
+  spaces = ' ' * n
+
+  if isinstance(df.columns.nlevels, pd.MultiIndex):
+    for i in range(df.columns.nlevels):
+      levelNew = [spaces + str(s) for s in df.columns.levels[i]]
+      df.columns.set_levels(levelNew, level=i, inplace=True)
+  else:
+    df.columns = spaces + df.columns
+
+  df = df.astype(str)
+  df = spaces + df
+
+  return df
 
 def impressao(df):
 
-  # relatorio = f"""
-  # ACME Inc.                   Uso do espaço em disco pelos usuários
-  # -----------------------------------------------------------------
-  # Nr.          Usuário          Espaço utilizado          % do uso
-  # {df[0][1]}
-
+  header = """
+  ACME Inc.             Uso do espaço em disco pelos usuários
+  -----------------------------------------------------------
+  \n"""
+  
+  f = open('relatorio.txt', 'x')
+  f.write(header)
+  f.close
+  
 
   relatorio = pd.DataFrame(columns=['Nr.', ' Usuário',  'Espaço utilizado',  '% do uso'])
 
@@ -49,9 +67,26 @@ def impressao(df):
   
   for i in range(usuarios):
     relatorio.loc[i] = [i+1, df[0][i], str(byte_para_megabyte(df[1][i])) + " MB", str(percentual_de_uso(byte_para_megabyte(df[1][i]), espacoTotalOcupado)) + " %"]
- 
+
+  relatorio = prefixar_espaços_nas_colunas(relatorio, 3)
   
-  print(relatorio)
+  relatorio = tabulate(relatorio, showindex=False, headers=relatorio.columns, tablefmt= "plain", stralign= ("right"))
+
+  espacoMedio = espacoTotalOcupado/usuarios
+  espacoMedio = float("{:.2f}".format(espacoMedio))
+
+  
+  f = open('relatorio.txt', 'a')
+  f.write(relatorio)
+  f.write(f"""
+    
+    Espaço total ocupado: {espacoTotalOcupado} MB
+    Espaço médio ocupado: {espacoMedio} MB""")
+  f.close
+
+  f = open('relatorio.txt', 'r')
+  print(f.read())
+  f.close
   
 
   
